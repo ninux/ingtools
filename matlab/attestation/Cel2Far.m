53333d33db5fcb6d443bc903dda8ba2091c27505
Nino Ninux <nino.ninux@gmail.com>
2013-05-05 11:47:45 +0200

    made some corrections on the documentation of Cel2Far

--- matlab/attestation/Cel2Far.m
+++ matlab/attestation/Cel2Far.m
@@ -2,10 +2,10 @@ function Temp=Cel2Far(cel, exact)
 
 % Cel2Far is a simple calculator to get a given temperature in Celsius 
 % converted to Farenheit.
-% Function: Cel2Far(Temperature in Celsius)
+% Function: Cel2Far(cel, exact)
 % Input: 
 %	cel	Temperature in degree Celsius (float)
-%	exact	if set the function returns float numbers, else integers
+%	exact	if seti, the function returns float numbers, else integers
 % Output: Temperature in degree Farenheit as int or float (see 'exact')
 % Example: Cel2Far(28)
 % Developer: Ervin Mazlagic
